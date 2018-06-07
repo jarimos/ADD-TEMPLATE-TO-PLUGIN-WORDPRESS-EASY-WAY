215effc0684b0663935c50b1419540e7a6bbdb48
jarimos <jarimortega@gmail.com>
2018-06-07 21:03:07 +0200

    Update code.php

--- code.php
+++ code.php
@@ -1,10 +1,11 @@
 //ADD TEMPLATE TO PLUGIN WORDPRESS EASY WAY
 
-add_filter( 'page_template', 'add_template_to_page_my_own_function' );
 function add_template_to_page_my_own_function( $page_template )
 {
     if ( is_page( 'my-custom-page-slug' ) ) {
-        $page_template = dirname( __FILE__ ) . '/custom-page-template.php';
+        $page_template = dirname( __FILE__ ) . '/my-custom-page-template.php';
     }
     return $page_template;
 }
+
+add_filter( 'page_template', 'add_template_to_page_my_own_function' );
